a6550de2e325c23367c0afe015cba7f31a91e30b
Zli123123 <67983161+Zli123123@users.noreply.github.com>
2021-02-19 14:18:01 -0500

    Update sketch.js

--- snowman/sketch.js
+++ snowman/sketch.js
@@ -3,5 +3,13 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
+  rect(0, 350, 400, 50);
+  ellipse(200, 300, 150, 150);
+  ellipse(200, 200, 100, 100);
+  ellipse(200, 120, 75, 75);
+  line(160, 200, 50, 50);
+  line(240, 200, 350, 50);
+  noLoop();
+
+
 }
